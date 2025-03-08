e61df0539cd8d46ab7671cdbc93ca369b2112b53
Satyajit Roy <aitlsatyajit@gmail.com>
2025-03-08 12:20:25 +0600

    update live

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,6 +1,6 @@
 export const META_API_URL = "https://graph.facebook.com/v21.0";
-export const PAGE_ID = "431751180032336";
-export const PAGE_ACCESS_TOKEN = "EAATriRmScZB4BO3neXIic8XKxDMG7ZAkuicOdDEaz9Urq4ZBsltVkrimnvhFgYZAZBaUk0MoYhgpbu9PyX0OqF1ZAozEWjuK0ftaGKmWZAbCE46MTfZB2TKYEagAKPZBdZCEWOqFCteLZCncZChkKJThM5J4e4c0B8QsLbQiwmHZC9b5ze0MaE4ws1MUB4mgPkVKJYIZAe";
+export const PAGE_ID = "111074608141788";
+export const PAGE_ACCESS_TOKEN = "EAAcV4TL9BBQBO6jUZCnkHq98zHozZCCWq01bIojDQt2aVZAVEHXzNAHZBa7zaBKC49k4PMZBdNIq7FazHFBlXnnMxHZBtssRM0BwMK63whVGbBr3tdSC89CelkKZCUoEPlk29B1e3ZBZCPMrPRU4dlt3BEyTixkxVFDGm8SfpbjZBBlsbcL41BUDfwZBPgXye9g0x5ghe9fQwcDxlT2";
 
-export const NEXT_PUBLIC_API_BASE_URL="https://test.api.prantosaha.com/public/api";
+export const NEXT_PUBLIC_API_BASE_URL="https://edutunedqm.com/api";
 export const NEXT_PUBLIC_API_TOKEN="fhdhfdkjsfdskjfskgfsdkfguerhfwreuew776787ruqiwrvivdcgiuasdfasdfgaisufhd7867";
