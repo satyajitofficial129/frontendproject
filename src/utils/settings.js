9da75ff9a7d5ebecddcc0bec277658c7df46fae8
Satyajit Roy <aitlsatyajit@gmail.com>
2025-03-11 14:33:45 +0600

    update page access token

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,6 +1,6 @@
 export const META_API_URL = "https://graph.facebook.com/v21.0";
 export const PAGE_ID = "111074608141788";
-export const PAGE_ACCESS_TOKEN = "EAAcV4TL9BBQBO6jUZCnkHq98zHozZCCWq01bIojDQt2aVZAVEHXzNAHZBa7zaBKC49k4PMZBdNIq7FazHFBlXnnMxHZBtssRM0BwMK63whVGbBr3tdSC89CelkKZCUoEPlk29B1e3ZBZCPMrPRU4dlt3BEyTixkxVFDGm8SfpbjZBBlsbcL41BUDfwZBPgXye9g0x5ghe9fQwcDxlT2";
+export const PAGE_ACCESS_TOKEN = "EAAcV4TL9BBQBO7E1F8JkbXmQPaE9SmqAYAjtccWa9RuXyk2FCGxM5JKS63UYbil3YqZBHbj25N2YlO5gLlpn4LNrxlmP7xBf5tJxfMccCIwkTfc8JHeiK1TppZBtSZBcvZAwDLSikQDO2aszJHIKANJaYSDEDjSdMhBEK1DPVIiVJC2QQt7H0uznHNxZCIw2W55BKsql5eZBRW";
 
 export const NEXT_PUBLIC_API_BASE_URL="https://edutunedqm.com/api";
 export const NEXT_PUBLIC_API_TOKEN="fhdhfdkjsfdskjfskgfsdkfguerhfwreuew776787ruqiwrvivdcgiuasdfasdfgaisufhd7867";
